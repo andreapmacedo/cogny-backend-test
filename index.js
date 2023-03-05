7cc18ccf1367f9407354a18a0908d71d68dcf09e
andreapmacedo <aa.andremacedo@gmail.com>
2023-03-04 21:07:00 -0300

    Feat: resolução do desafio.

--- index.js
+++ index.js
@@ -66,6 +66,57 @@ const axios = require('axios');
     try {
         await migrationUp();
 
+        const apiUrl = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+        
+        // const response = await axios.get(apiUrl);
+        // const data = response.data;
+        
+        
+        // const data = await axios.get(apiUrl);
+        const data = {"data":[{"ID Nation":"01000US","Nation":"United States","ID Year":2020,"Year":"2020","Population":326569308,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2019,"Year":"2019","Population":324697795,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2018,"Year":"2018","Population":322903030,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2017,"Year":"2017","Population":321004407,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2016,"Year":"2016","Population":318558162,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2015,"Year":"2015","Population":316515021,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2014,"Year":"2014","Population":314107084,"Slug Nation":"united-states"},{"ID Nation":"01000US","Nation":"United States","ID Year":2013,"Year":"2013","Population":311536594,"Slug Nation":"united-states"}],"source":[{"measures":["Population"],"annotations":{"source_name":"Census Bureau","source_description":"The American Community Survey (ACS) is conducted by the US Census and sent to a portion of the population every year.","dataset_name":"ACS 5-year Estimate","dataset_link":"http://www.census.gov/programs-surveys/acs/","table_id":"B01003","topic":"Diversity","subtopic":"Demographics"},"name":"acs_yg_total_population_5","substitutions":[]}]}
+        
+        // console.log('data:', data);
+        // console.log('data:', data.data);
+
+
+        const sqlSelect = `SELECT * FROM ${DATABASE_SCHEMA}.api_data`;
+        const sqlQuerySelect = await db.query(sqlSelect);
+        console.log('Selected data:', sqlQuerySelect);
+
+        const populate = async () => {
+            if (sqlQuerySelect.length > 0) {
+                return
+            };
+            data.data.map((item) => {
+                db[DATABASE_SCHEMA].api_data.insert({doc_record: item });
+            });
+        }
+          
+        populate();
+   
+        // a. em memoria no nodejs
+        const filteredData = data.data.filter(item => item.Year === '2020' || item.Year === '2019' || item.Year === '2018');
+        const totalPopulation = filteredData.reduce((acc, val) => acc + val.Population, 0);
+        console.log('Total population in years 2020, 2019 and 2018 (using memory on nodejs):', totalPopulation);
+
+        // c. usando SELECT no postgres, pode fazer uma VIEW no banco de dados.
+        const view = `
+        CREATE OR REPLACE VIEW population_sum AS
+        SELECT SUM((doc_record->>'Population')::int) as total_population
+        FROM ${DATABASE_SCHEMA}.api_data
+        WHERE doc_record->>'Year' IN ('2020', '2019', '2018');
+        `;
+
+        const sqlQuerySetView = await db.query(view);
+        const sqlQueryView = `SELECT * FROM population_sum;`;
+        const sqlQuerySelectView = await db.query(sqlQueryView);
+
+        console.log('Total population in years 2020, 2019 and 2018 (using view in PostgreSQL):', sqlQuerySelectView);
+
+        // b. usando SELECT no postgres, pode fazer um SELECT inline no nodejs.
+        db.query('SELECT * FROM population_sum;').then((data) => {
+            console.log('Total population in years 2020, 2019 and 2018 (using select inline):',  data);
+        });
 
     } catch (e) {
         console.log(e.message)
